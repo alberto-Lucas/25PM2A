ce45547be27a5b94e15a4be43a826d9bac45727a
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2025-08-11 22:14:30 -0300

    Animacoes

--- AppAnimacoes/MainPage.xaml.cs
+++ AppAnimacoes/MainPage.xaml.cs
@@ -54,6 +54,70 @@
         {
             imgTeste.ScaleTo(imgTeste.Scale - 0.5, 250);
         }
+
+        private void btnFade_Clicked(object sender, EventArgs e)
+        {
+            //Opacity é a propriade que define
+            //o quão transparente é a imagem
+
+            //Resetar a opacidade
+            imgTeste.Opacity = 1;
+            imgTeste.FadeTo(0, 1000);
+            //A imagem ficara 100% transparente
+
+            //Agora iremos reverter
+            //Resetando a opacidade em trasnparente
+            imgTeste.Opacity = 0;
+            imgTeste.FadeTo(1, 1000);
+        }
+
+        private void btnTremer_Clicked(object sender, EventArgs e)
+        {
+            Tremer(50);
+        }
+
+        //Método para geração animação de "tremida"
+        //Precisa ser do tupo async
+        //pois precisamos executar e aguardar
+        //cada animação
+        //Usar o parametro UINT para nao aceitar
+        //valores negativos
+        async void Tremer(uint tempo)
+        {
+            //A ideia da animação de tremida
+            //é mover a imagem para direita e para esquerda
+            //reduzindo a distancia de movimento a cada conjunto
+            //Primeiro parametro eixo x tremer horizontal
+            //Segundo parametro eixo y tremer vertical
+            await imgTeste.TranslateTo(15, 0, tempo);
+            await imgTeste.TranslateTo(-15, 0, tempo);
+            await imgTeste.TranslateTo(10, 0, tempo);
+            await imgTeste.TranslateTo(-10, 0, tempo);
+            await imgTeste.TranslateTo(5, 0, tempo);
+            await imgTeste.TranslateTo(-5, 0, tempo);
+            imgTeste.TranslationX = 0;
+        }
+
+        private async void btnGirarZoom_Clicked(object sender, EventArgs e)
+        {
+            //Executar duas animações ou mais
+            //ao mesmo tempo
+            //Para isso iremos usar um recurso
+            //asincrono para excução simultanea
+
+            imgTeste.Rotation = 0;
+            //Agora iremos juntar duas animações
+            //girar para direita e zoom + ao mesmo tempo
+            await Task.WhenAny<bool>
+            (
+                //Agora colocar as animações
+                imgTeste.RotateTo(360, 2000),
+                imgTeste.ScaleTo(2, 1000)
+                
+            );
+            //Remover o zoom
+            imgTeste.ScaleTo(1, 1000);
+        }
     }
 
 }
