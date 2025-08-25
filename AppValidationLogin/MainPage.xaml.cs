7c2fc0a141cba777cd79b994e1ffc7acb0d07394
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2025-08-25 20:01:10 -0300

    AppValidationLogin

--- AppValidationLogin/MainPage.xaml.cs
+++ AppValidationLogin/MainPage.xaml.cs
@@ -7,9 +7,28 @@
             InitializeComponent();
         }
 
+        //Método tremer
+        //para aplicar animação no componente informado
+        private async void TremerEntry(Entry txtCampo)
+        {
+            uint tempo = 50;
+
+            await txtCampo.TranslateTo(-15, 0, tempo);
+            await txtCampo.TranslateTo(15, 0, tempo);
+            await txtCampo.TranslateTo(-10, 0, tempo);
+            await txtCampo.TranslateTo(10, 0, tempo);
+            await txtCampo.TranslateTo(-5, 0, tempo);
+            await txtCampo.TranslateTo(5, 0, tempo);
+            txtCampo.TranslationX = 0;
+        }
+
         private void btnEntrar_Clicked(object sender, EventArgs e)
         {
+            if (txtEmail.Text != "admin")
+                TremerEntry(txtEmail);
 
-        }
+            if (txtSenha.Text != "admin")
+                TremerEntry(txtSenha);
+        }   
     }
 }
