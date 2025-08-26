503616c295396a04c65dcd3b77fcd84fe91af68b
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2025-08-25 21:59:37 -0300

    AppValidationLogin

--- AppValidationLogin/MainPage.xaml.cs
+++ AppValidationLogin/MainPage.xaml.cs
@@ -22,8 +22,37 @@
 
         private void btnEntrar_Clicked(object sender, EventArgs e)
         {
-            ValidarCampo(txtEmail, lblValidationEmail, "admin");
-            ValidarCampo(txtSenha, lblValidationSenha, "admin");
+            //Tipo 1 de Validação
+            //ValidarCampo(txtEmail, lblValidationEmail, "admin");
+            //ValidarCampo(txtSenha, lblValidationSenha, "admin");
+
+            //Tipo 2 de validação usando a classe Validation
+            //Primeiro precisando instanciar a classe
+            //e vincular os componentes
+            Validation Email = new Validation(txtEmail, lblValidationEmail);
+            Validation Senha = new Validation(txtSenha, lblValidationSenha);
+
+            //Criar as validações personalizadas
+        }
+
+        private bool ValidarEmail(Validation Email)
+        {
+            //Definir o resultado padrão
+            bool resultado = false;
+
+            if (Email.IsEmpty()) //Valido se está vazio
+                Email.SetValidation("* Informe o Email.", true);
+            else if (Email.GetText().Length < 5) //Se possui 5 ou mais caracteres
+                Email.SetValidation("* Informe um email com no minimo 5 caracteres.", true);
+            else if (!Email.Equals("admin")) //Se o conteudo é diferente de admin
+                Email.SetValidation("* Informe um Email válido.", true);
+            else
+            {
+                resultado = true;
+                Email.HideValidation();
+            }
+
+            return resultado;
         }
     }
 }
---
+++ AppValidationLogin/Validation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppValidationLogin
+{
+    public class Validation
+    {
+        //Definir as propriedades referente
+        //ao nosso "PAR" de componentes
+        private Entry EntryCampo { get; set; }
+        private Label LabelValidation { get; set; }
+
+        //Criar o contrustor para fazer o vinculo
+        public Validation(Entry txtCampo, Label lblValidation)
+        {
+            EntryCampo = txtCampo;
+            LabelValidation = lblValidation;
+        }
+
+        //Método para retorno o conteudo do Entry
+        public string GetText()
+        { 
+            return EntryCampo.Text; 
+        }
+
+        //Método para definir o texto da label validation
+        public void SetValidation(string msgValidation)
+        {
+            LabelValidation.Text = msgValidation;
+            LabelValidation.IsVisible = true;
+        }
+
+        //Método de sobrecarga para aplicar animação no validation
+        public void SetValidation(string msgValidation, bool ExibirTremer)
+        {
+            if (ExibirTremer)
+                Animation.Tremer(EntryCampo);
+            SetValidation(msgValidation);
+        }
+
+        //Método para ocultar a label validation
+        public void HideValidation()
+        {
+            LabelValidation.IsVisible = false;
+        }
+
+        //Método que retorna se o campo está vazio
+        public bool IsEmpty()
+        {
+            return string.IsNullOrEmpty(EntryCampo.Text.Trim());
+        }
+
+        //Método que retorna se o conteudo do campo igual a X texto
+        public bool IsEquals(string valor)
+        {
+            return EntryCampo.Equals(valor);
+        }
+    }
+}
