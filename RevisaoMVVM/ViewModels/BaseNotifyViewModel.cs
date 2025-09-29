05422ffe4db8c9a1a865794ccf4209be0f00d35b
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2025-09-29 20:06:11 -0300

    RevisaoMVVM
    RevisaoMVVM

---
+++ RevisaoMVVM/ViewModels/BaseNotifyViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisaoMVVM.ViewModels
+{
+    //Classe padrão para invocar as notificações
+    //de alteração das propriedades e atualizar as telas
+    //de acordo com a documentação C#
+    public class BaseNotifyViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(
+            [CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke
+                (this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
