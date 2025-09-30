a2e37f5e6d4d0e93ee5e1c81190a383fd0a534f8
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2025-09-29 21:45:18 -0300

    RevisaoMVVM

--- RevisaoMVVM/Views/CarroVisualizacaoView.xaml.cs
+++ RevisaoMVVM/Views/CarroVisualizacaoView.xaml.cs
@@ -1,9 +1,25 @@
+using RevisaoMVVM.ViewModels;
+
 namespace RevisaoMVVM.Views;
 
 public partial class CarroVisualizacaoView : ContentPage
 {
 	public CarroVisualizacaoView()
 	{
-		InitializeComponent();
+        //Importar a camada de ViewModels
+        //using Nome_Projeto.ViewModels;
+        InitializeComponent();
+
+        //Ao abrir a tela, irá carregar dados
+        //automaticamente
+        //para isso precisamos chamar o
+        //ConsultarCommand direto
+
+        //Recuperar a instancia da ViewModel
+        var carroViewModel = new CarroViewModel();
+        //Vinculo a tela a viewModel
+        BindingContext = carroViewModel;
+        //Executar o ConsultarCommand direto
+        carroViewModel.ConsultarCommand.Execute(null);
 	}
 }
