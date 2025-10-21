5890b31a3b7bb7ccb5567de891549fb5c27bad67
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2025-10-20 21:43:53 -0300

    LoginMVVM

---
+++ LoginMVVM/Services/UsuarioService.cs
@@ -0,0 +1,20 @@
+﻿using LoginMVVM.Models;
+
+namespace LoginMVVM.Services
+{
+    public class UsuarioService
+    {
+        FakeDBSingleton fakeDB = 
+            FakeDBSingleton.Instancia;
+
+        public void Inserir(Usuario usuario)
+        {
+            fakeDB.Usuario = usuario;
+        }
+
+        public Usuario GetUsuario()
+        {
+            return fakeDB.Usuario;
+        }
+    }
+}
---
+++ LoginMVVM/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,66 @@
+﻿using LoginMVVM.Models;
+using LoginMVVM.Services;
+
+namespace LoginMVVM.ViewModels
+{
+    public class UsuarioViewModel : BaseViewModel
+    {
+        UsuarioService usuarioService =
+            new UsuarioService();
+
+        private string _nome;
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                _nome = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _cpf;
+        public string Cpf
+        {
+            get { return _cpf; }
+            set
+            {
+                _cpf = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _senha;
+        public string Senha
+        {
+            get { return _senha; }
+            set
+            {
+                _senha = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _dtNascimento;
+        public string DtNascimento
+        {
+            get { return _dtNascimento; }
+            set
+            {
+                _dtNascimento = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
